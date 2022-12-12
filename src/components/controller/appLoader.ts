42038fc7bc3f2b024f99cc860e551d1b97e4f330
Kunitsa Olya <o.kunitsa@itransition.com>
2022-12-12 06:20:46 +0300

    feat: add ts settings

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '94d75239dac2483192d785ebf3fee1a2', // получите свой ключ https://newsapi.org/
         });
     }
 }
